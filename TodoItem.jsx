653466f526ff4aa1a0ec73f3faf7b40591673714
jennifer566 <141760598+jennifer566@users.noreply.github.com>
2023-12-19 13:23:07 -0500

    Add files via upload

---
+++ TodoItem.jsx
@@ -0,0 +1,17 @@
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={e => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        <id className="fas fa-trash-alt trash-icon"></id>
+      </button>
+    </li>
+  )
+}
